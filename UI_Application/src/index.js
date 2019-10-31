857c855e8babd3ae05cbdc1a99524e7444916b45
SK851831 <sk851831@gmail.com>
2019-10-31 23:54:44 +0530

    initial code setup done

---
+++ UI_Application/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <div><h1>Welcome Team Inputs...</h1></div>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
